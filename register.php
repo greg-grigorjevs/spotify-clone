0ecb1737f5e4e4f53f8184dc0d7787fe49de366c
grihasmp5 <grihasmp5@icloud.com>
2020-08-21 21:12:16 +0300

    complete video 32

--- register.php
+++ register.php
@@ -30,12 +30,26 @@
 
 <body>
     
-    <script>
-    	$(document).ready(function() {
-	    $("#loginForm").hide();
-	    $("#registerForm").show();
-	});
-    </script>
+<?php
+    if(isset($_POST['registerButton'])) {
+      echo '<script>
+            $(document).ready(function() {
+            $("#loginForm").hide();
+            $("#registerForm").show();
+        });
+        </script>';
+    }
+    else {
+      echo '<script>
+            $(document).ready(function() {
+            $("#loginForm").show();
+            $("#registerForm").hide();
+        });
+        </script>';
+        
+    }
+?>        
+
     <div id="background">
         <div id="loginContainer">
             <div id="inputContainer">
