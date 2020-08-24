7e65e5a0aeaa81cd1b1b153632609699316bac8d
grihasmp5 <grihasmp5@icloud.com>
2020-08-24 21:53:48 +0300

    add styling to index page

--- index.php
+++ index.php
@@ -12,12 +12,17 @@ if(isset($_SESSION['userLoggedIn'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
- <h1>Hello world</h1> 
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+
+        <div id="nowPlayingBarContainer">
+            
+        </div>
+
+    </body>
 </html>
