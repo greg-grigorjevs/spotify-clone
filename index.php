1b7bf2716ee7ea289026ca3818ccf6db1579bd7f
grihasmp5 <grihasmp5@icloud.com>
2020-08-25 19:47:22 +0300

    complete video 43. add control buttons

--- index.php
+++ index.php
@@ -26,6 +26,34 @@ if(isset($_SESSION['userLoggedIn'])) {
                 <div id="nowPlayingLeft">
                 </div> 
                 <div id="nowPlayingCenter">
+                    <div class="content playerControls">
+                        <div class="buttons">
+                            <button class="controlButton" title="Shuffle button">
+                                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                            </button>
+
+                            <button class="controlButton" title="Previous button">
+                                <img src="assets/images/icons/previous.png" alt="Previous">
+                            </button>
+
+                            <button class="controlButton" title="Play button">
+                                <img src="assets/images/icons/play.png" alt="Play">
+                            </button>
+
+                            <button class="controlButton" title="Pause button">
+                                <img src="assets/images/icons/pause.png" alt="Pause" style="display: none">
+                            </button>
+
+                            <button class="controlButton" title="Next button">
+                                <img src="assets/images/icons/next.png" alt="Next">
+                            </button>
+
+                            <button class="controlButton" title="Repeat button">
+                                <img src="assets/images/icons/repeat.png" alt="Repeat">
+                            </button>
+
+                        </div>
+                    </div>
                 </div> 
                 <div id="nowPlayingRigth">
                 </div> 
