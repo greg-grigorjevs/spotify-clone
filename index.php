b23dc61155c5ffadaf04d2220a68f0bc5b38b0b0
grihasmp5 <grihasmp5@icloud.com>
2020-08-25 22:05:03 +0300

    complete video 44

--- index.php
+++ index.php
@@ -28,27 +28,27 @@ if(isset($_SESSION['userLoggedIn'])) {
                 <div id="nowPlayingCenter">
                     <div class="content playerControls">
                         <div class="buttons">
-                            <button class="controlButton" title="Shuffle button">
+                            <button class="controlButton shuffle" title="Shuffle button">
                                 <img src="assets/images/icons/shuffle.png" alt="Shuffle">
                             </button>
 
-                            <button class="controlButton" title="Previous button">
+                            <button class="controlButton previous" title="Previous button">
                                 <img src="assets/images/icons/previous.png" alt="Previous">
                             </button>
 
-                            <button class="controlButton" title="Play button">
+                            <button class="controlButton play" title="Play button">
                                 <img src="assets/images/icons/play.png" alt="Play">
                             </button>
 
-                            <button class="controlButton" title="Pause button">
+                            <button class="controlButton pause" title="Pause button">
                                 <img src="assets/images/icons/pause.png" alt="Pause" style="display: none">
                             </button>
 
-                            <button class="controlButton" title="Next button">
+                            <button class="controlButton next" title="Next button">
                                 <img src="assets/images/icons/next.png" alt="Next">
                             </button>
 
-                            <button class="controlButton" title="Repeat button">
+                            <button class="controlButton repeat" title="Repeat button">
                                 <img src="assets/images/icons/repeat.png" alt="Repeat">
                             </button>
 
