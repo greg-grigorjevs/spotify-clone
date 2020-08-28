d1d2d51c37b6e51192ae6b4d5c747022b670a038
grihasmp5 <grihasmp5@icloud.com>
2020-08-28 22:33:13 +0300

    complete video 49

--- index.php
+++ index.php
@@ -87,6 +87,18 @@ if(isset($_SESSION['userLoggedIn'])) {
                     </div>
                 </div> 
                 <div id="nowPlayingRight">
+                    <div class="volumeBar">
+                        <button class="controlButton volume">
+                            <img src="assets/images/icons/volume.png" alt="Volume">
+                        </button>
+
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div> 
             </div>
 
