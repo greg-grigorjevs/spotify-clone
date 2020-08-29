b4a0a7a1f25a378ed6a5e04bc947781c1453fd2f
RasselCrow <grihasmp5@icloud.com>
2020-08-29 11:53:25 +0300

    complete video 54 - Navigation search icon

--- index.php
+++ index.php
@@ -32,7 +32,9 @@ if(isset($_SESSION['userLoggedIn'])) {
 
                             <div class="group">
                                 <div class="navItem">
-                                    <a href="search.php" class="navItemLink">Search</a>
+                                    <a href="search.php" class="navItemLink">Search
+                                        <img src="assets/images/icons/search.png" class="icon" alt="Search Icon">
+                                    </a>
                                 </div>
                             </div>
 
