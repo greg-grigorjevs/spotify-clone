65a30e10a8748dbae89d3c5feb212f05816b818c
RasselCrow <grihasmp5@icloud.com>
2020-08-31 01:46:08 +0300

    complete video 77.

--- album.php
+++ album.php
@@ -26,4 +26,21 @@ $artist = $album->getArtist();
     </div>
 </div>
 
+<div class="tracklistContainer">
+    <ul class="tracklist">
+
+<?php
+
+$songIdArray = $album->getSongIds();
+foreach($songIdArray as $songId) {
+    echo $songId . "<br>";
+}
+
+?>
+
+
+
+    </ul>
+</div>
+
 <?php include("includes/footer.php"); ?>
