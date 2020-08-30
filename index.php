3ff4445405f301f46f314353a10c170c6c738a31
RasselCrow <grihasmp5@icloud.com>
2020-08-30 15:05:43 +0300

    complete video 59. add music and artwork to file and database

--- index.php
+++ index.php
@@ -21,42 +21,42 @@ if(isset($_SESSION['userLoggedIn'])) {
     <body>
 
         <div id="mainContainer">
-            <div id="nowPlayingBarContainer">
 
-                <div id="topContainer">                    
-                    <div id="navBarContainer">
-                        <nav class="navBar">
-                            <a href="index.php" class="logo">
-                                <img src="assets/images/icons/logo.png" alt="">
-                            </a>
+            <div id="topContainer">                    
+                <div id="navBarContainer">
+                    <nav class="navBar">
+                        <a href="index.php" class="logo">
+                            <img src="assets/images/icons/logo.png" alt="">
+                        </a>
 
-                            <div class="group">
-                                <div class="navItem">
-                                    <a href="search.php" class="navItemLink">Search
-                                        <img src="assets/images/icons/search.png" class="icon" alt="Search Icon">
-                                    </a>
-                                </div>
+                        <div class="group">
+                            <div class="navItem">
+                                <a href="search.php" class="navItemLink">Search
+                                    <img src="assets/images/icons/search.png" class="icon" alt="Search Icon">
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="group">
+
+                            <div class="navItem">
+                                <a href="browse.php" class="navItemLink">Browse</a>
                             </div>
 
-                            <div class="group">
-                                
-                                <div class="navItem">
-                                    <a href="browse.php" class="navItemLink">Browse</a>
-                                </div>
-
-                                <div class="navItem">
-                                    <a href="yourMusic.php" class="navItemLink">Your Music</a>
-                                </div>
-
-                                <div class="navItem">
-                                    <a href="profile.php" class="navItemLink">Shrek</a>
-                                </div>
-                                
+                            <div class="navItem">
+                                <a href="yourMusic.php" class="navItemLink">Your Music</a>
                             </div>
-                        </nav>
-                    </div>
+
+                            <div class="navItem">
+                                <a href="profile.php" class="navItemLink">Shrek</a>
+                            </div>
+
+                        </div>
+                    </nav>
                 </div>
+            </div>
 
+            <div id="nowPlayingBarContainer">
                 <div id="nowPlayingBar">
                     <div id="nowPlayingLeft">
                         <div class="content">
