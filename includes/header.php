d0435d83959a50b501d1d46cc0894b96c35c645d
RasselCrow <grihasmp5@icloud.com>
2020-08-30 16:00:50 +0300

    complete video 63. add header and footer files

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+
+<?php
+include("includes/config.php");
+
+if(isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+    header("Location: register.php");
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+
+        <div id="mainContainer">
+
+            <div id="topContainer">                    
+                <?php include("includes/navBarContainer.php"); ?>
+            </div>
+
+            <div id="mainViewContainer">
+                <div id="mainContent">
+                
+                
