945f27e103b8c9b82d25aa625630e2bcf8559612
RasselCrow <grihasmp5@icloud.com>
2020-08-31 16:48:25 +0300

    complete video 86

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function Audio() {
+    this.currentlyPlaying;
+    this.audio = document.createElement("audio");
+
+    this.setTrack = (src) => {
+        this.audio.src = src;
+    };
+}
--- includes/header.php
+++ includes/header.php
@@ -21,8 +21,11 @@ if(isset($_SESSION['userLoggedIn'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="assets/js/script.js"></script>
     </head>
     <body>
+
 
         <div id="mainContainer">
 
