40fd17121a1e0018791a660e1b86151f90c6cb8e
RasselCrow <grihasmp5@icloud.com>
2020-09-02 21:05:26 +0300

    complete video 104

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,69 @@
 var currentPlaylist = new Array();
 var audioElement;
+var mousedown = false;
+// index of the currently playing song
+let currentIndex = 0;
+let repeat = false;
 
+const formatTime = (duration) => {
+    const time = Math.round(duration);
+    const minutes = Math.floor(time / 60);
+    const seconds = time - (minutes * 60);
+    let extraZero = "";
+
+    if (seconds < 10) {
+        extraZero = "0";
+    }
+
+    return minutes + ":" + extraZero + seconds;
+}
+
+const updateTimeProgressBar = (audio) => {
+    $(".progressTime.current").text(formatTime(audio.currentTime));
+    $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+    const progress = audio.currentTime / audio.duration * 100;
+    $(".playbackBar .progress").css("width", progress + "%");
+}
+
+const updateVolumeProgressBar = audio => {
+    const volume = audio.volume * 100;
+    $(".volumeBar .progress").css("width", volume + "%");
+}
 
 function Audio() {
     this.currentlyPlaying;
     this.audio = document.createElement("audio");
 
-    this.setTrack = (src) => {
-        this.audio.src = src;
+    this.audio.addEventListener("canplay", function() {
+        const duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
+    })
+
+    this.audio.addEventListener("timeupdate", function() {
+        if (this.duration) {
+            updateTimeProgressBar(this);
+        }
+    })
+
+    this.audio.addEventListener("volumechange", function() {
+        updateVolumeProgressBar(this);
+    })
+
+    this.setTrack = (track) => {
+        this.audio.src = track.path;
+        this.currentlyPlaying = track;
     };
 
     this.play = () => {
         this.audio.play();
+    }
+
+    this.pause = () => {
+        this.audio.pause();
+    }
+
+    this.setTime = (seconds) => {
+        this.audio.currentTime = seconds;
     }
 }
--- includes/nowPlayingBarContainer.php
+++ includes/nowPlayingBarContainer.php
@@ -15,17 +15,133 @@ $jsonArray = json_encode($resultArray);
 
 $(document).ready(function() {
     currentPlaylist = <?php echo $jsonArray; ?>;
+    //create our audio object
     audioElement = new Audio();
     setTrack(currentPlaylist[0], currentPlaylist, false);
+    updateVolumeProgressBar(audioElement.audio);
+
+    $("#nowPlayingBarContainer").on("mousedown mousemove touchstart touchmove", e => {
+        e.preventDefault();
+    })
+
+    $(".playbackBar .progressBar").mousedown(function() {
+        mousedown = true;
+    })
+
+    $(".playbackBar .progressBar").mousemove((e) => {
+        // set time of the song depending on mouse position
+        if (mousedown) {
+            // for some reason $(this) this does not work 
+            var progressBar = $(".playbackBar .progressBar");
+            timeFromOffset(e, progressBar); 
+        }
+    })
+
+    $(".playbackBar .progressBar").mouseup((e) => {
+        var progressBar = $(".playbackBar .progressBar");
+       timeFromOffset(e, progressBar); 
+    })
+
+    $(".volumeBar .progressBar").mousedown(function() {
+        mousedown = true;
+    })
+
+    $(".volumeBar .progressBar").mousemove(function(e) {
+        // set volume depending on mouse position
+        if (mousedown) {
+            const percentage = e.offsetX / $(this).width();
+            if (percentage >= 0 && percentage <= 1) {
+                audioElement.audio.volume = percentage;
+            }
+        }
+    })
+
+    $(".volumeBar > .progressBar").mouseup((e) => {
+        // if I am using arrow function $(this) would not work
+        // so I have to use e.currentTarget
+        if (mousedown) {
+            const percentage = e.offsetX / $(e.currentTarget).width();
+            if (percentage >= 0 && percentage <= 1) {
+                audioElement.audio.volume = percentage;
+            }
+        }
+    })
+
+    $(document).mouseup(() => {
+       mousedown = false; 
+    })
+
 })
 
-function setTrack(trackId, newPlaylist, play) {
-    audioElement.setTrack("assets/music/bensound-goinghigher.mp3");
-    
-    if(play) {
-        audioElement.play();
-        
+function timeFromOffset(mouse, progressBar) {
+    var percentage = mouse.offsetX / $(progressBar).width() * 100;
+    var seconds = audioElement.audio.duration * (percentage / 100);
+    audioElement.setTime(seconds);
+}
+
+function nextSong() {
+    if (repeat) {
+        audioElement.setTime(0);
+        playSong();
+        return;
     }
+    if (currentIndex == currentPlaylist.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+
+    const trackToPlay = currentPlaylist[currentIndex];
+    setTrack(trackToPlay, currentPlaylist, true);
+}
+
+function setRepeat() {
+    repeat = !repeat;
+    const imageName = repeat ? "repeat-active.png" : "repeat.png";
+    $(".controlButton.repeat img").attr("src", "assets/images/icons/" + imageName);
+}
+
+function setTrack(trackId, newPlaylist, play) {
+    $.post("includes/handlers/ajax/getSongJson.php", {songId: trackId}, function(data) {
+        const track = JSON.parse(data);
+        $(".trackName span").text(track.title);
+        currentIndex = currentPlaylist.indexOf(trackId);
+
+        // get Artist name
+        $.post("includes/handlers/ajax/getSongArtist.php", {artistId: track.artist}, function(data) {
+            const artist = JSON.parse(data);
+            $(".artistName span").text(artist.name);
+        })
+
+        // get Album's cover
+        $.post("includes/handlers/ajax/getAlbumJson.php", {albumId: track.album}, function(data) {
+           const album = JSON.parse(data);
+           $(".albumLink img").attr("src", album.artworkPath);
+        })
+        audioElement.setTrack(track);
+        if(play) {
+            playSong();
+            audioElement.play();
+            
+        }
+
+    })
+    
+}
+
+function playSong() {
+    if (audioElement.audio.currentTime == 0) {
+        $.post("includes/handlers/ajax/updatePlays.php", {songId: audioElement.currentlyPlaying.id});
+    }
+    $(".controlButton.play").hide();
+    $(".controlButton.pause").show();
+    audioElement.play()
+}
+
+function pauseSong() {
+    $(".controlButton.play").show();
+    $(".controlButton.pause").hide();
+    audioElement.pause();
 }
 
 </script>
@@ -35,18 +151,17 @@ function setTrack(trackId, newPlaylist, play) {
         <div id="nowPlayingLeft">
             <div class="content">
                 <span class="albumLink">
-                    <img src="https://i.insider.com/5c59e77ceb3ce80d46564023?width=1100&format=jpeg&auto=webp"
-                         class="albumArtwork">
+                    <img src="" class="albumArtwork">
                 </span>
 
                 <div class="trackInfo">
 
                     <span class="trackName">
-                        <span>All Star</span>
+                        <span></span>
                     </span>
 
                     <span class="artistName">
-                        <span>Smash Mouth</span>
+                        <span></span>
                     </span>
 
                 </div>
@@ -63,19 +178,19 @@ function setTrack(trackId, newPlaylist, play) {
                         <img src="assets/images/icons/previous.png" alt="Previous">
                     </button>
 
-                    <button class="controlButton play" title="Play button">
+                    <button class="controlButton play" title="Play button" onclick="playSong()">
                         <img src="assets/images/icons/play.png" alt="Play">
                     </button>
 
-                    <button class="controlButton pause" title="Pause button" style="display: none">
+                    <button class="controlButton pause" title="Pause button" style="display: none" onclick="pauseSong()">
                         <img src="assets/images/icons/pause.png" alt="Pause" >
                     </button>
 
-                        <button class="controlButton next" title="Next button">
+                        <button class="controlButton next" title="Next button" onclick="nextSong()">
                             <img src="assets/images/icons/next.png" alt="Next">
                         </button>
 
-                        <button class="controlButton repeat" title="Repeat button">
+                        <button class="controlButton repeat" title="Repeat button" onclick="setRepeat()">
                             <img src="assets/images/icons/repeat.png" alt="Repeat">
                         </button>
 
